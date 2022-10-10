4aa305dbd803fdbeceff030e780eea97270b7461
Ambush <56769633+ambushneupane@users.noreply.github.com>
2022-10-10 15:02:31 +0545

    GUI Version of Hangman (#7)
    * GUI Version of Hangman
    
    * Increased Words

---
+++ hangman-GUI/wordsStore.py
@@ -0,0 +1,3 @@
+
+words=["Developer","Ambush","Reddit","Food","PYGAME","PYthon","hacking","reading","fast","mango","coding","books","mother","Grand","Evening","Music","Love","Happy","Agony","Visual","request","realm","Information","Laugh","Calender","Promo","Speaker","Base","Nose","Eyes","Sugar","Couch","Coach","Lovely","Mirror","Clear","King","Name","Mouse"]
+words=[word.upper() for word in words]
