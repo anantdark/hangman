5eb1a0776eef8024a87e6a1622e86ce90a720d7b
Abiodun Akinola <akinolaemmanuel49@gmail.com>
2022-10-10 13:57:32 +0100

    Unify Wordlist

--- hangman-cli/hangman.py
+++ hangman-cli/hangman.py
@@ -1,14 +1,21 @@
-from wordgen import *
+
+import sys
+
+sys.path.append("..")
+
+from words.wordgen import *  # NOQA
+
 
 def hangman(word):
     lives = 6
     board = "*"*len(word)
     while lives:
-        print(f'You have {lives} lives. Guess a word. Current board\n{board}') 
+        print(f'You have {lives} lives. Guess a word. Current board\n{board}')
         guess = input()
         # If guess is more than 1 character, prompt to try again
         while len(guess) != 1:
-            print(f"You can only guess with 1 character\nYou still have {lives} lives. Guess again!")
+            print(
+                f"You can only guess with 1 character\nYou still have {lives} lives. Guess again!")
             guess = input()
         # If character exist in word, then don't lose a life
         character_exist = False
@@ -22,14 +29,13 @@ def hangman(word):
         print(board)
         # If any character is missing, continue (go back to top of while loop
         # and skipping win prompt
-        completed_word = True;
+        completed_word = True
         for char in board:
             if char == "*":
-                completed_word = False;
+                completed_word = False
                 break
         if completed_word:
-            break;
-        
+            break
 
     if lives == 0:
         print(f'You lost all your lives :( The word is {word}')
@@ -37,9 +43,6 @@ def hangman(word):
         print(f'You win! The word is {word}')
 
 
-
-
-
-if __name__=="__main__":
+if __name__ == "__main__":
     current_word = random_word()
     hangman(current_word)
